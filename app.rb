5e1c2c5e90da05b9480473d1f0904cde5170f752
DDuongNguyen <duong.nguyen4991@gmail.com>
2019-06-03 18:09:12 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,21 @@
+# frozen_string_literal: true
+
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
-	get '/' do
-		erb :index
-	end
+  get '/hello' do
+    erb :hello
+  end
 
+  get '/goodbye' do
+    @name = 'Joe'
+    erb :goodbye
+  end
 
+  get '/date' do
+    @time = Date.today.strftime('%A, %B %d, %Y')
+    erb :date
+  end
 end
